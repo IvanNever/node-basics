ce346922847a05be7b08811a2907faa950018da0
Ivan Neverovsky <ivan@neverovsky.com.ua>
2020-09-03 17:32:49 +0300

    node.js basics

---
+++ refs/os_ref.js
@@ -0,0 +1,22 @@
+const os = require('os')
+
+//Platform
+console.log(os.platform())
+
+//Architecture
+console.log(os.arch())
+
+//Information
+console.log(os.cpus())
+
+//Free memory
+console.log(os.freemem())
+
+//Total memory
+console.log(os.totalmem())
+
+//Root directory
+console.log(os.homedir())
+
+//Time of system worked
+console.log(os.uptime())
